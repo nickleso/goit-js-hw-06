ff925ed456532b364ec20c01fbfab6a75e03fc09
Nick Leso <nickleso.work@gmail.com>
2022-08-14 16:43:22 +0300

    Task 10 completed

--- js/task-10.js
+++ js/task-10.js
@@ -1,77 +1,51 @@
-// функция генерации рандомного цвета
+// color randomizer
 function getRandomHexColor() {
   return `#${Math.floor(Math.random() * 16777215).toString(16)}`;
 }
 
-// селектор панели
+// selectors
 const controlPanel = document.querySelector('#controls');
 
-// input
 const inputEl = controlPanel.firstElementChild;
-let numberOfBoxesToCreate = 10;
-
-inputEl.addEventListener('input', onInputValue);
-
-function onInputValue() {
-  numberOfBoxesToCreate = Number(inputEl.value);
-  console.log(numberOfBoxesToCreate);
-  return;
-}
-
-inputEl.style.backgroundColor = 'bisque';
-
-// количество боксов
-
-console.log(numberOfBoxesToCreate);
-
-// селекторы кнопок и контейнера
 const createBtn = document.querySelector('button[data-create]');
 const destroyBtn = document.querySelector('button[data-destroy]');
+
 const divColoredBoxContainer = document.querySelector('#boxes');
 
-// функция создания бокса
 const elements = [];
 
-for (let i = 0; i < numberOfBoxesToCreate; i += 1) {
-  const divColoredBox = document.createElement('div');
+inputEl.addEventListener('select', () => {});
 
-  divColoredBoxContainer.classList.add('boxes-container');
-
-  divColoredBox.classList.add('boxes-container__item');
-  divColoredBox.style.backgroundColor = getRandomHexColor();
-
-  const boxGeometryCounter = Number(30 + i * 10);
-  divColoredBox.style.width = boxGeometryCounter + 'px';
-  divColoredBox.style.height = boxGeometryCounter + 'px';
-
-  elements.push(divColoredBox);
-}
-
-// function makeColoredBox() {
-//   const divColoredBox = document.createElement('div');
-//   divColoredBox.classList.add('.color-box');
-//   divColoredBox.style.backgroundColor = getRandomHexColor();
-//   divColoredBox.style.marginTop = '10px';
-//   divColoredBox.style.width = boxGeometryCounter + 'px';
-//   divColoredBox.style.height = '30px';
-
-//   return divColoredBox;
-// }
-
-// const elements = makeColoredBox();
-
-// создание бокса
+// create boxes
 createBtn.addEventListener('click', function () {
+  for (let i = 0; i < Number(inputEl.value); i += 1) {
+    const divColoredBox = document.createElement('div');
+
+    divColoredBoxContainer.classList.add('boxes-container');
+
+    divColoredBox.classList.add('boxes-container__item');
+    divColoredBox.style.backgroundColor = getRandomHexColor();
+
+    const boxGeometryCounter = Number(30 + i * 10);
+    divColoredBox.style.width = boxGeometryCounter + 'px';
+    divColoredBox.style.height = boxGeometryCounter + 'px';
+
+    elements.push(divColoredBox);
+
+    // console.log(elements);
+  }
+
   divColoredBoxContainer.append(...elements);
-  console.log('Boxes created !');
+  console.log(`Boxes created: ${inputEl.value}`);
 });
 
-// удаление боксов
+// destroy boxes
 function destroyBoxes() {
-  elements.remove();
+  divColoredBoxContainer.innerHTML = '';
+  elements.splice(0, 101);
 }
 
 destroyBtn.addEventListener('click', function () {
   destroyBoxes();
-  console.log('Boxes destroyed !');
+  console.log('Boxes destroyed!');
 });
